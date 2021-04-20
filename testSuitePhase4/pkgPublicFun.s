3dd10e3309f6fd99da4b2ac52c8f3682a6dad2cb
Rick McKenny <11lrm1@queensu.ca>
2021-04-19 20:33:21 -0400

    pkg public functions

---
+++ testSuitePhase4/pkgPublicFun.s
@@ -0,0 +1,26 @@
+    .data    
+    .comm    u,12
+n:    .long    0
+    .text    
+    .globl    ptmain
+ptmain:    pushl   %ebp
+        movl    %esp, %ebp    
+    movl    $2,u+0
+    jmp    f1
+p13:    pushl   %ebp
+        movl    %esp, %ebp    
+    movl    8(%ebp),%eax
+    movl    %eax,u+4
+    movl    $5,n
+    movl    $2,u+4
+    leave    
+    ret    
+f1:    movl    $1,u+8
+    movl    $9,n
+    pushl    u+8
+    call    p13
+    addl    $4,%esp
+    movl    $9,n
+    call    pttrap0
+    leave    
+    ret    
